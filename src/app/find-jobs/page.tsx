3ace7fa1fea52810caaab61f9d2da7db6bee251f
abyankamal <kamalmuhammad414@gmail.com>
2024-02-25 22:35:47 +0700

    slicing find jobs page

--- src/app/find-jobs/page.tsx
+++ src/app/find-jobs/page.tsx
@@ -1,6 +1,41 @@
+"use client";
+
 import ExploreDataContainer from "@/containers/ExploreDataContainer";
+import { formFilterSchema } from "@/lib/form-schema";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { filterFormType } from "@/types";
+import { CATEGORIES_OPTIONS } from "@/constants";
+
+const FILTER_FORMS: filterFormType[] = [
+  {
+    name: "categories",
+    label: "categories",
+    items: CATEGORIES_OPTIONS,
+  },
+];
 
 export default function FindJobsPage() {
-  return <ExploreDataContainer />;
+  const formFilter = useForm<z.infer<typeof formFilterSchema>>({
+    resolver: zodResolver(formFilterSchema),
+  });
+
+  const onSubmitFormFilter = async (val: z.infer<typeof formFilterSchema>) => {
+    console.log(val);
+  };
+  return (
+    <ExploreDataContainer
+      formFilter={formFilter}
+      onSubmitFilter={onSubmitFormFilter}
+      filterForms={filters}
+      title="dream job"
+      subtitle="Find your next career at companies like HubSpot, Nike,
+			and Dropbox"
+      loading={false}
+      type="job"
+      data={jobs}
+    />
+  );
 }
