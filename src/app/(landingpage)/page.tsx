212caa1d28ee26e204c23c4da18bbbacf8686896
abyankamal <kamalmuhammad414@gmail.com>
2024-06-25 15:51:59 +0700

    final build 3

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <div className="absolute w-2/3 h-screen top-0 right-0 -z-10">
         <Image src="/images/pattern.png" alt="/images/pattern.png" fill />
       </div>
-      <div>{t("test")}</div>
       <div className="px-32 mb-10">
         <Hero />
         <Clients />
