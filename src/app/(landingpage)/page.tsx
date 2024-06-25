cf2c9e261a4a2cad0eb6558c96bc2e58903c9392
abyankamal <kamalmuhammad414@gmail.com>
2024-06-25 16:12:13 +0700

    final build 3

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -6,23 +6,26 @@ import Clients from "@/components/organisms/Clients";
 import FeaturedJobs from "@/components/organisms/FeaturedJobs";
 import Hero from "@/components/organisms/Hero";
 import LatestJobs from "@/components/organisms/LatestJobs";
-import { t } from "i18next";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <div className="absolute w-2/3 h-screen top-0 right-0 -z-10">
-        <Image src="/images/pattern.png" alt="/images/pattern.png" fill />
-      </div>
-      <div className="px-32 mb-10">
-        <Hero />
-        <Clients />
-        <Category />
-        <BannerSignUp />
-        <FeaturedJobs />
-        <LatestJobs />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="absolute w-2/3 h-screen top-0 right-0 -z-10">
+				<Image
+					src="/images/pattern.png"
+					alt="/images/pattern.png"
+					fill
+				/>
+			</div>
+			<div className="px-32 mb-10">
+				<Hero />
+				<Clients />
+				<Category />
+				<BannerSignUp />
+				<FeaturedJobs />
+				<LatestJobs />
+			</div>
+		</>
+	);
 }
